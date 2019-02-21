34643dc5904aa5724110fcb32efd3409c3eaddac
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-02-21 08:21:30 -0500

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,52 @@
+class Book
+  def initialize(title) #Hook/Callback
+    @title = title
+  end
 
+  def title
+    @title #gets a property
+  end
 
+  def author=(author_name) #setter(prop)
+    @author = author_name
+  end
+
+  def author #getter(prop)
+    @author
+  end
+
+  def page_count=(pages)
+    @page_count = pages
+  end
+
+  def page_count
+    @page_count
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def turn_page
+   puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,47 @@
 # Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color
+    @color
+  end
+
+  def size
+    @size
+  end
+
+  def size=(size)
+    @size = size
+  end
+
+  def material #getter
+    @material
+  end
+
+  def material=(material) #setter
+    @material = material
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def condition #getter
+    @condition
+  end
+
+  def condition=(condition) #getter
+    @condition = condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
