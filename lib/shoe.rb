e7449071897b0707c40f1281742c4d3f16949053
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-02-21 13:22:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,4 +1,5 @@
 # Make your shoe class here!
+<<<<<<< HEAD
 class Shoe
   def initialize(brand)
     @brand = brand
@@ -45,3 +46,18 @@ class Shoe
     @condition = "new"
   end
 end
+=======
+class Shoe 
+  def initialize(brand)
+    @brand = brand 
+  end 
+  
+  def brand
+    @brand 
+  end 
+  
+  def color=(color)
+    @color = color  
+  end 
+end 
+>>>>>>> 1c821bb3be8f1f461740c5c58c1f0f51526fd413
